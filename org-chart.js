1d165b785e5a3be9991bde20670874f8fd2aefc5
Taeil Kwak <taeil2@gmail.com>
2020-07-13 17:15:07 -0500

    created org chart

---
+++ org-chart.js
@@ -0,0 +1,76 @@
+let orgChart = {
+  ceo: 'Zuckerberg',
+  team1: {
+    leader: 'Schroepfer',
+    group1: {
+      head: 'Bosworth',
+      members: {
+        member1: 'Steve',
+        member2: 'Kyle',
+        member3: 'Andra',
+      }
+    },
+    group2: {
+      head: 'Zhao',
+      members: {
+        member1: 'Richie',
+        member2: 'Sofia',
+        member3: 'Jen',
+      }
+    },
+  },
+  team2: {
+    leader: 'Schrage',
+    group1: {
+      head: 'VanDyck',
+      members: {
+        member1: 'Sabrina',
+        member2: 'Michelle',
+        member3: 'Josh',
+      }
+    },
+    group2: {
+      head: 'Swain',
+      members: {
+        member1: 'Blanch',
+        member2: 'Tom',
+        member3: 'Joe',
+      }
+    },
+  },
+  team3: {
+    leader: 'Sandberg',
+    group1: {
+      head: 'Goler',
+      members: {
+        member1: 'Eddie',
+        member2: 'Julie',
+        member3: 'Annie',
+      }
+    },
+    group2: {
+      head: 'Hernandez',
+      members: {
+        member1: 'Rowi',
+        member2: 'Inga',
+        member3: 'Morgan',
+      }
+    },
+    group3: {
+      head: 'Moissinac',
+      members: {
+        member1: 'Amy',
+        member2: 'Chuck',
+        member3: 'Vinni',
+      }
+    },
+    group4: {
+      head: 'Kelley',
+      members: {
+        member1: 'Eric',
+        member2: 'Ana',
+        member3: 'Wes',
+      }
+    },
+  }
+};
