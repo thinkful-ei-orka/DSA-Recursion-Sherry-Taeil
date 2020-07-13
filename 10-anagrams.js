8142aa5637fe5b87dc0b65c42a71fc2f67ffd465
Sherry McCarty <sherrydmccarty@gmail.com>
2020-07-13 19:52:07 -0400

    org chart finished

--- 10-anagrams.js
+++ 10-anagrams.js
@@ -1,11 +1,13 @@
 function anagrams(word, permutations = null) {
   let newPermutations = [];
-  // console.log(permutations);
+  console.log('perms', permutations);
 
   if (permutations === null) {
     for (let i = 0; i < word.length; i++) {
       let remainingLetters = word.slice(0, i) + word.slice(i + 1, word.length);
+      console.log('remaining letters', remainingLetters)
       newPermutations.push([word[i], remainingLetters])
+      console.log('newP', newPermutations)
     }
   } else {
     // if there are no remaining letters, return the result
@@ -18,10 +20,14 @@ function anagrams(word, permutations = null) {
       return completePermutations;
     } else { // create a new set of permutations otherwise
       permutations.forEach(permutation => {
+        console.log('permutation on line 23', permutation)
         let remainingLetters = permutation[1];
+        console.log('other remaining letters', remainingLetters)
         for (let i = 0; i < remainingLetters.length; i++) {
           let newRemainingLetters = remainingLetters.slice(0, i) + remainingLetters.slice(i + 1, remainingLetters.length);
+          console.log('new Remaining letters', newRemainingLetters)
           newPermutations.push([permutation[0] + remainingLetters[i], newRemainingLetters])
+          console.log('new perms', newPermutations)
         }
       });
     }
---
+++ 11-organization-chart.js
@@ -0,0 +1,92 @@
+function orgChart(obj, level=0) {
+  let chart = '';
+  let numOfSpaces = level * 4;
+  for (let key in obj) {
+    if(typeof obj[key] !== 'object') {
+      chart += `
+${Array(numOfSpaces).fill(' ').join('')}${obj[key]}`;
+    } else {
+      chart += orgChart(obj[key], level + 1);
+    }
+  }
+  return chart;
+}
+
+let org = {
+  ceo: 'Zuckerberg',
+  team1: {
+    leader: 'Schroepfer',
+    group1: {
+      head: 'Bosworth',
+      members: {
+        member1: 'Steve',
+        member2: 'Kyle',
+        member3: 'Andra',
+      }
+    },
+    group2: {
+      head: 'Zhao',
+      members: {
+        member1: 'Richie',
+        member2: 'Sofia',
+        member3: 'Jen',
+      }
+    },
+  },
+  team2: {
+    leader: 'Schrage',
+    group1: {
+      head: 'VanDyck',
+      members: {
+        member1: 'Sabrina',
+        member2: 'Michelle',
+        member3: 'Josh',
+      }
+    },
+    group2: {
+      head: 'Swain',
+      members: {
+        member1: 'Blanch',
+        member2: 'Tom',
+        member3: 'Joe',
+      }
+    },
+  },
+  team3: {
+    leader: 'Sandberg',
+    group1: {
+      head: 'Goler',
+      members: {
+        member1: 'Eddie',
+        member2: 'Julie',
+        member3: 'Annie',
+      }
+    },
+    group2: {
+      head: 'Hernandez',
+      members: {
+        member1: 'Rowi',
+        member2: 'Inga',
+        member3: 'Morgan',
+      }
+    },
+    group3: {
+      head: 'Moissinac',
+      members: {
+        member1: 'Amy',
+        member2: 'Chuck',
+        member3: 'Vinni',
+      }
+    },
+    group4: {
+      head: 'Kelley',
+      members: {
+        member1: 'Eric',
+        member2: 'Ana',
+        member3: 'Wes',
+      }
+    },
+  }
+};
+
+orgChart(org);
